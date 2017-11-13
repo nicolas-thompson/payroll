9cd930673be789b6efe49cf95f283f9e72e50f2c
Nicolas Thompson <mail@nicolasthompson.com>
2017-11-13 22:30:52 +0000

    Refactoring - created private function figureOutTheSalaryDate()

--- src/Payroll.php
+++ src/Payroll.php
@@ -10,7 +10,7 @@ class Payroll{
 	 * @param string $password
 	 * @return string $hash
 	 */
-	public static function payDates( $year ){
+	public function payDates( $year ){
 
 		$months = range(1,12);
 		$rows = [];
@@ -18,17 +18,23 @@ class Payroll{
         {
 			$dateObj   = \DateTime::createFromFormat('!m', $month);
 			$monthName = $dateObj->format('F');
-			$salaryDay = date("Y-m-t", strtotime($year . '-' . $month . '-' . '15'));
-			$weekday = intval(date("N", strtotime($salaryDay)));
-			$salaryDay = $weekday == 6 ? date('Y-m-d', strtotime('-1 day', strtotime($salaryDay))) : $salaryDay;
-			$salaryDay = $weekday == 7 ? date('Y-m-d', strtotime('-2 day', strtotime($salaryDay))) : $salaryDay;
+			$salaryDate = $this->figureOutTheSalaryDate($year, $month);
 			$firstExpensesDay =  date("Y-m-d", strtotime($year .'-' . $month . '-' . 1));
 			$weekday = intval(date("N", strtotime($firstExpensesDay)));
 			$firstExpensesDay = $weekday == 6 ? date('Y-m-d', strtotime('+2 day', strtotime($firstExpensesDay))) : $firstExpensesDay;
 			$firstExpensesDay = $weekday == 7 ? date('Y-m-d', strtotime('+1 day', strtotime($firstExpensesDay))) : $firstExpensesDay; 
-			$rows[] = [ $monthName, $firstExpensesDay, $salaryDay ];
+			$rows[] = [ $monthName, $firstExpensesDay, $salaryDate ];
         }
 
 		return $rows;
+	}
+
+	private function figureOutTheSalaryDate($year, $month)
+	{
+		$salaryDay = date("Y-m-t", strtotime($year . '-' . $month . '-' . '15'));
+		$weekday = intval(date("N", strtotime($salaryDay)));
+		$salaryDay = $weekday == 6 ? date('Y-m-d', strtotime('-1 day', strtotime($salaryDay))) : $salaryDay;
+		$salaryDay = $weekday == 7 ? date('Y-m-d', strtotime('-2 day', strtotime($salaryDay))) : $salaryDay;
+		return $salaryDay;
 	}
 }
